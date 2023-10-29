05cb04f36e890dfc2af802beaa5befd52b0c976d
jiangshuyang1 <3216702682@qq.com>
2023-10-29 18:40:50 +0800

    Default Changelist

--- src/example/Test.java
+++ src/example/Test.java
@@ -1,18 +1,21 @@
 package example;
 
-import example.client.CalTaxServiceLocator;
-import example.client.CalTax_PortType;
+//import example.client.CalTaxServiceLocator;
+//import example.client.CalTax_PortType;
+
+import example.client2.IncomeTax;
+import example.client2.IncomeTaxServiceLocator;
 
 public class Test {
     public static void main(String[] argv) {
         try {
-            CalTaxServiceLocator locator = new CalTaxServiceLocator();
-            CalTax_PortType service = locator.getCalTax();
+            IncomeTaxServiceLocator locator = new IncomeTaxServiceLocator();
+            IncomeTax service = locator.getCalTax();
             // If authorization is required
             //((HelloWorldSoapBindingStub)service).setUsername("user3");
             //((HelloWorldSoapBindingStub)service).setPassword("pass3");
             // invoke business method
-            System.out.println(service.calTax("12000"));
+            System.out.println(service.calTax("55000"));
         } catch (javax.xml.rpc.ServiceException ex) {
             ex.printStackTrace();
         } catch (java.rmi.RemoteException ex) {
